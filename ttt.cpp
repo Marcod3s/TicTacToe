7723e6aa5910a406705400500735cad767818c2e
Marcod3s <kronikntity@gmail.com>
2026-07-18 23:55:22 -0400

    added potential way to end game with endGame() function

--- ttt.cpp
+++ ttt.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 #include <vector>
-
+ 
 
 class board3x3{
     private:
         int matrix[3][3] = {};
     public:
 
-        void checkBoard(){
+        bool checkBoard(){
             for(int i = 0; i < 3; i++){
                 if((matrix[i][0] == matrix[i][1]) && (matrix[i][1] == matrix[i][2])){
-                    //game end function
+                    return true;
                 }
             }
             for(int j = 0; j < 3; j++){
                 if((matrix[0][j] == matrix[1][j]) && (matrix[1][j] == matrix[2][j])){
-                    //game end function
+                    return true;
                 }
                 }
             
             if ((matrix[0][0] == matrix[1][1]) && (matrix[1][1] == matrix[2][2])){
-                //game end function
+                return true;
             }
             if ((matrix[0][2] == matrix[1][1]) && matrix[1][1] == matrix[2][0]) {
-                //game end function
+                return true;
             }
+            return false;
         }            
         void put(int r, int c, int player){
-            //prompt something about inputting your pick
+            //prompt something about inputting where you'll put your x/o
             if (matrix[r][c] == 0){
                 matrix[r][c] = player;
             }
@@ -36,13 +37,19 @@ class board3x3{
             }
 
 
+
         }
+
+        void gameEnd();
 
 };
 
 void p1turn(board3x3 board, int r, int c){
 
     board.put(r,c,1);
+    if(board.checkBoard() == false){
+        board.gameEnd();
+    }
 
 }
 
@@ -55,6 +62,7 @@ void p2turn(board3x3 board, int r, int c){
 
 int main(){
 
+    
 
 }
 
